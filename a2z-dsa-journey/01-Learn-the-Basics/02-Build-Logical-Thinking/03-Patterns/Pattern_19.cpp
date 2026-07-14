00c495581b02b67eb16cd194234cbe343444b614
Ayushi Gaur <ayushi.gaur@vamhi.com>
2026-07-15 01:33:29 +0530

    15-07-2026 : Pattern Print

---
+++ a2z-dsa-journey/01-Learn-the-Basics/02-Build-Logical-Thinking/03-Patterns/Pattern_19.cpp
@@ -0,0 +1,64 @@
+// // Given an integer n. You need to recreate the pattern given below for any value of N.
+//  Let's say for N = 5, the pattern should look like as below:
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number" << endl;
+    cin >> n;
+
+    // Upper half
+    for (int i = 0; i < n; i++) {
+        // Left stars
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        // Spaces
+        for (int j = 0; j < 2 * (n - i - 1); j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = 1; i < n; i++) {
+        // Left stars
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        // Spaces
+        for (int j = 0; j < 2 * i; j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
